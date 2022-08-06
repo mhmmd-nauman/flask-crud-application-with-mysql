8232e96a34d2028eca2e2b5e56060707fb457800
Dr. Muhammad Nauman <mhmmd.nauman@gmail.com>
2022-08-06 14:19:51 +0500

    first program

---
+++ fp.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = 'many-secret-key'
+
+@app.route('/')
+def index():
+     return render_template('first-program.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
